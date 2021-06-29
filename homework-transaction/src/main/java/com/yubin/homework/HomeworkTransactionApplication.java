8b2980988305c86b5b37ad2c798fd5167c40b67e
余斌 <yubin02@kanzhun.com>
2021-06-29 21:46:16 +0800

    update

--- homework-transaction/src/main/java/com/yubin/homework/HomeworkTransactionApplication.java
+++ homework-transaction/src/main/java/com/yubin/homework/HomeworkTransactionApplication.java
@@ -3,7 +3,6 @@ package com.yubin.homework;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @Slf4j
